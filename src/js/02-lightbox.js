0cf5fbfb210ecfd87a41037e113740cec4298517
Kravtsov <kravtsovofficial99@gmail.com>
2022-07-18 10:50:35 +0300

    finish tasc 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createDiv(array) {
+    return array.reduce((acc, item) => acc + `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`, "")
+}
+
+const div = document.querySelector(".gallery")
+div.insertAdjacentHTML("beforeend", createDiv(galleryItems) )
+
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt"
+})
